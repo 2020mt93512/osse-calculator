37a00aec47ddabb5e38b5451240e916574e758d1
tanya93574 <2020mt93574@wilp.bit-pilani.ac.in>
2022-05-07 14:54:07 +0530

    Added compute function in calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -221,6 +221,32 @@ class Calculator {
 		}
 		this._repaintDisplay();
 	}
+
+	compute() {
+		let computation
+		const prev = parseFloat(this.previousOperand)
+		const current = parseFloat(this.currentOperand)
+		if (isNaN(prev) || isNaN(current)) return
+		switch (this.operation) {
+			case '+':
+				computation = prev + current
+				break
+			case '-':
+				computation = prev - current
+				break
+			case '*':
+				computation = prev * current
+				break
+			case '÷':
+				computation = prev / current
+				break
+			default:
+				return
+		}
+		this.currentOperand = computation
+		this.operation = undefined
+		this.previousOperand = ''
+	}
 }
 
 
